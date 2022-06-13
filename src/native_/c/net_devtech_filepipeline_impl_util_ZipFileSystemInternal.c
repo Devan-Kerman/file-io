e5ccccce6c680b69e408f432446c60f0cebf53c8
Devan-Kerman <dev.sel20@gmail.com>
2022-06-12 23:28:15 -0500

    make jni work

--- src/native_/c/net_devtech_filepipeline_impl_util_ZipFileSystemInternal.c
+++ src/native_/c/net_devtech_filepipeline_impl_util_ZipFileSystemInternal.c
@@ -3,7 +3,7 @@
 #include "net_devtech_filepipeline_impl_util_ZipFileSystemInternal.h"
 
 JNIEXPORT
-void JNICALL Java_net_devtech_filepipeline_impl_util_ZipFileSystemInternal_sync(JNIEnv * env, jclass, jobject zipfs) {
+void JNICALL Java_net_devtech_filepipeline_impl_util_ZipFileSystemInternal_sync(JNIEnv * env, jclass zipfsInternal, jobject zipfs) {
   jclass cls_foo = (*env)->GetObjectClass(env, zipfs);
   jmethodID mid_callback = (*env)->GetMethodID(env, cls_foo, "sync", "()V");
   (*env)->CallVoidMethod(env, zipfs, mid_callback);
